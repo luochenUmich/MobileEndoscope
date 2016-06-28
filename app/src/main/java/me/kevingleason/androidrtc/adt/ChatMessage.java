4257d18fd5f24218c72e60fa9eb61c1d3b34e4ca
Chen Luo <luochen@umich.edu>
2016-06-28 20:32:00 +0800

    First Commit

---
+++ app/src/main/java/me/kevingleason/androidrtc/adt/ChatMessage.java
@@ -0,0 +1,30 @@
+package me.kevingleason.androidrtc.adt;
+
+public class ChatMessage {
+    private String sender;
+    private String message;
+    private long timeStamp;
+
+    public ChatMessage(String sender, String message, long timeStamp){
+        this.sender = sender;
+        this.message = message;
+        this.timeStamp=timeStamp;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    @Override
+    public int hashCode() {
+        return (this.sender + this.message + this.timeStamp).hashCode();
+    }
+}
---
+++ app/src/main/java/me/kevingleason/androidrtc/util/Constants.java
@@ -0,0 +1,30 @@
+package me.kevingleason.androidrtc.util;
+
+public class Constants {
+    public static final String SHARED_PREFS = "me.kg.androidrtc.SHARED_PREFS";
+    public static final String USER_NAME    = "me.kg.androidrtc.SHARED_PREFS.USER_NAME";
+    public static final String CALL_USER    = "me.kg.androidrtc.SHARED_PREFS.CALL_USER";
+    public static final String STDBY_SUFFIX = "-stdby";
+
+    public static final String PUB_KEY = "pub-c-ac285b02-6959-4644-aa11-52f2b4c996e9"; // Your Pub Key
+    public static final String SUB_KEY = "sub-c-7508a5dc-2d40-11e6-8b91-02ee2ddab7fe"; // Your Sub Key
+
+//    public static final String PUB_KEY = "demo"; // Your Pub Key
+//    public static final String SUB_KEY = "demo"; // Your Sub Key
+
+    public static final String JSON_CALL_USER = "call_user";
+    public static final String JSON_CALL_TIME = "call_time";
+    public static final String JSON_OCCUPANCY = "occupancy";
+    public static final String JSON_STATUS    = "status";
+
+    // JSON for user messages
+    public static final String JSON_USER_MSG  = "user_message";
+    public static final String JSON_MSG_UUID  = "msg_uuid";
+    public static final String JSON_MSG       = "msg_message";
+    public static final String JSON_TIME      = "msg_timestamp";
+
+    public static final String STATUS_AVAILABLE = "Available";
+    public static final String STATUS_OFFLINE   = "Offline";
+    public static final String STATUS_BUSY      = "Busy";
+
+}
